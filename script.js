d8226f0d3966f23682aa887beda27e5139c47643
Muneer Girnari <muneeragirnari@gmail.com>
2024-09-30 01:05:13 +0530

    modified js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+let html = document.getElementsByTagName("html");
+
+let body = document.getElementsByTagName("body")[0];
+
+let ul = document.getElementsByTagName("ul")[0];
+
+let element = document.getElementsByTagName("li")[4];
+
+
+function domLevel(){
+    let level = 1;
+    while(element.parentElement){
+        level++;
+        element = element.parentElement;
+    }
+    return level;
+}
+
+alert("The Level of Element is " + domLevel());
